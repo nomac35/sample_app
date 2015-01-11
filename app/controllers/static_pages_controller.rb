1f9957bea92f2295a9c4b896130dc7056bffe29f
Alper <apinar@gmail.com>
2015-01-11 00:43:07 -0800

    css work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def help
+  	@title = "help"
   end
 
   def about
+  	@title = "about"
   end
+
+  def contact
+  	@title = 'contact'
+  end
+  
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,11 @@
 module StaticPagesHelper
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{@title} | #{base_title}"
+		end
+	end
+
 end
